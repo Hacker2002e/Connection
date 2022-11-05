0ba54a089297cdd5ec71da058400f0574afcf5f2
User <User@SHIZOFREN>
2022-11-05 11:50:25 +0400

    Add project files.

---
+++ Connection/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Connection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Con = new System.Windows.Forms.Button();
+            this.lblConnection = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Con
+            // 
+            this.btn_Con.BackColor = System.Drawing.Color.Blue;
+            this.btn_Con.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Con.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Con.Location = new System.Drawing.Point(283, 289);
+            this.btn_Con.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btn_Con.Name = "btn_Con";
+            this.btn_Con.Size = new System.Drawing.Size(303, 88);
+            this.btn_Con.TabIndex = 0;
+            this.btn_Con.Text = "Connect";
+            this.btn_Con.UseVisualStyleBackColor = false;
+            this.btn_Con.Click += new System.EventHandler(this.btn_Con_Click);
+            // 
+            // lblConnection
+            // 
+            this.lblConnection.AutoSize = true;
+            this.lblConnection.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblConnection.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConnection.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblConnection.Location = new System.Drawing.Point(422, 137);
+            this.lblConnection.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblConnection.Name = "lblConnection";
+            this.lblConnection.Size = new System.Drawing.Size(152, 30);
+            this.lblConnection.TabIndex = 2;
+            this.lblConnection.Text = "Closed Now";
+            this.lblConnection.Click += new System.EventHandler(this.lblConnection_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(375, 167);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(355, 30);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "--------------------------------------";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 137);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(371, 40);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Current Connection State:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(815, 454);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblConnection);
+            this.Controls.Add(this.btn_Con);
+            this.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Con;
+        private System.Windows.Forms.Label lblConnection;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Connection/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connection
+{
+    public partial class Form1 : Form
+    { //"Server=localhost; Database=Northwind; Integrated Security=true;"
+        private SqlConnection conn = new SqlConnection("Server=SHIZOFREN\\SQLEXPRESS; Database=Northwind; Integrated Security=true;");
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Con_Click(object sender, EventArgs e)
+        {
+            
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+                btn_Con.Text = "Disconnect";
+                lblConnection.Text = "Connection opened";
+
+             }
+            else
+            {
+                conn.Close();
+                btn_Con.Text = "Connect";
+                lblConnection.Text = "Connection closed";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblConnection_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
